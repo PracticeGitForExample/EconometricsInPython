97aa562418899f25deecbdd57eb50a809b882cab
PracticeGitForExample <75341066+PracticeGitForExample@users.noreply.github.com>
2021-07-25 00:05:27 -0400

    Update articleserver.js

--- articleserver.js
+++ articleserver.js
@@ -8,7 +8,7 @@
 
 //} );
 
-const URL = 'https://github.com/PracticeGitForExample/EconometricsInPython/blob/main/blogpostex.html';
+const URL = 'https://github.com/PracticeGitForExample/EconometricsInPython/blogpostex.html';
 function getData(pageId) {
   console.log(pageId);
   var myRequest = new Request(pageId + '.txt');
