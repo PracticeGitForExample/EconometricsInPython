0780c2ccf49814777aecb71d72fd4a6fb48a33bd
PracticeGitForExample <75341066+PracticeGitForExample@users.noreply.github.com>
2021-07-24 23:59:34 -0400

    Update articleserver.js

--- articleserver.js
+++ articleserver.js
@@ -1,9 +1,22 @@
 //content.innerHTML='<object type="text/html" data="ClassLibraries.html" ></object>';
 ///content.fetch("js\ClassLibraries.html")).text();
 
-document.querySelector("iframe").addEventListener( "load", function(e) {
-    alert(this.nodeName);
+//document.querySelector("iframe").addEventListener( "load", function(e) {
+  //  alert(this.nodeName);
 
-    console.log(e.target);
+   // console.log(e.target);
 
-} );
+//} );
+
+const URL = 'https://practicegitforexample.github.io/EconometricsInPython/blogpostex.html';
+function getData(pageId) {
+  console.log(pageId);
+  var myRequest = new Request(pageId + '.txt');
+  fetch(myRequest).then(function(response) {
+    return response.text().then(function(text) {
+      content.innerHTML = text;
+    });
+  });
+};
+getData(URL);
+
